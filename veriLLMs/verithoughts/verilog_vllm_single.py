36839c55cc239b75c897e2f7c21884d0e49de644
Dimitrios Stamoulis <stamoulis.dimitrios@gmail.com>
2025-06-20 22:46:58 -0500

    fix

--- veriLLMs/verithoughts/verilog_vllm_single.py
+++ veriLLMs/verithoughts/verilog_vllm_single.py
@@ -16,6 +16,11 @@ from openai import OpenAI
 openai_api_key = "EMPTY"
 openai_api_base = "http://localhost:8000/v1"
 
+api_client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
 from verithoughts_utils import extract_code_block
 
 # OpenAI-compatible API service with vLLM
